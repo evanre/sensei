9b92a180820edc2a102bd1b45690d2a3609c206b
Illya Klymov <xanf@xanf.me>
2019-04-30 20:36:08 +0300

    Add CI Support

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@
 const globby = require("globby");
 const path = require("path");
 const fs = require("fs");
+const { exec } = require("child_process");
+const { promisify } = require("util");
 const sgf = require("staged-git-files");
 
 const fgRed = "\x1b[31m";
@@ -12,21 +14,38 @@ function log(message) {
   console.log(`${fgRed}${message}${reset}`);
 }
 
-const protectedConfig = path.resolve(process.cwd(), "./.jsninja/protected");
-if (!fs.existsSync(protectedConfig)) {
-  process.exit(0);
+async function getFromMaster() {
+  const execP = promisify(exec);
+  const { stdout } = await execP("git diff --name-status master~1..HEAD");
+  return stdout
+    .split("\n")
+    .filter(Boolean)
+    .map(t => t.split("\t")[1]);
 }
-const patterns = fs.readFileSync(protectedConfig, "utf-8").split("\n");
-sgf(async function r(err, results) {
+
+async function run() {
+  const [, , isCi] = process.argv;
+  const protectedConfig = path.resolve(process.cwd(), "./.jsninja/protected");
+  if (!fs.existsSync(protectedConfig)) {
+    console.log("No config found, exiting");
+    process.exit(0);
+  }
+  const patterns = fs.readFileSync(protectedConfig, "utf-8").split("\n");
   const protectedFiles = await globby(patterns);
-  const changedFiles = results.map(f => f.filename);
+
+  const changedFiles = isCi
+    ? await getFromMaster()
+    : (await promisify(sgf)()).map(f => f.filename);
+
   const badFiles = changedFiles.filter(f => protectedFiles.includes(f));
   if (badFiles.length) {
-    log("These files are protected. You can't commit them:");
+    log("These files are protected. You can't change them:");
     badFiles.forEach(f => {
       log(`  - ${f}`);
     });
     process.exit(1);
   }
   process.exit(0);
-});
+}
+
+run();
